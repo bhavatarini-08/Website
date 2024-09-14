dd83521269593b7f1dbd53c1202ac29d00452177
bhavatarini-08 <bhavatariniselvan@gmail.com>
2024-09-14 08:48:52 +0530

    form validation added

---
+++ es.js
@@ -0,0 +1,11 @@
+const function1 =()=> {
+   
+    console.log("Hi")
+}
+const function2 =()=> {for(let i=0;i<5;i++) console.log("Hello")}
+function1()
+function2()
+
+let a,b
+const add =(a,b)=> console.log(a+b)
+add(10,30)
